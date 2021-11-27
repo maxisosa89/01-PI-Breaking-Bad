07c5b427e043a2d529816bf1551e602e5d01a0a5
unknown <msosa89@outlook.com>
2021-11-27 15:59:09 -0300

    DB

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('character', {
+    char_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV1
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
-  });
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    birthday: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM('Alive', 'Deceased', 'Presumed dead', 'Unknown')
+    },
+    img: {
+      type: DataTypes.STRING
+    },
+    db: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {timestamps: false});
 };
