b04de2e09b4e86575e78e7a1dca94f1d9268d218
unknown <msosa89@outlook.com>
2021-11-28 14:23:04 -0300

    Backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
+const axios = require('axios');
+const { Character, Occupation } = require('../db');
+
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +12,119 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getApiInfo = async () => {
+    const apiUrl = await axios.get('https://www.breakingbadapi.com/api/characters')
+    const apiInfo = await apiUrl.data.map (el => {
+        const { char_id, name, nickname, birthday, status, img, occupation } = el;
+        return {
+            char_id,
+            name,
+            nickname,
+            birthday,
+            status,
+            img,
+            occupation
+        }
+    })
+    return apiInfo;
+}
+
+const getDbInfo = async () => {
+    return await Character.findAll({
+        include: {
+            model: Occupation,
+            attributes: ['name'],
+            through: {
+                attributes: [],
+            }
+        }
+    })
+}
+
+const getAllCharacters = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = dbInfo.concat(apiInfo);
+    return infoTotal;
+}
+
+router.get('/characters', async (req, res) => {
+    const name = req.query.name;
+    const charactersTotal = await getAllCharacters();
+    if (name) {
+        const characterName = await charactersTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
+        characterName.length ?
+        res.send(characterName) : 
+        res.status(404).send("Character not found")
+    } else {
+        res.send(charactersTotal)
+    }
+})
+
+router.get('/occupations', async (req, res) => {
+    const charactersTotal = await getAllCharacters();
+    const aux = await charactersTotal.map(el => el.occupation);
+    let occupationsApi = [];
+    //[ [], [], [] ]
+    for (let i = 0; i < aux.length; i++){
+        for (let j = 0; j < aux[i].length; j++){
+            occupationsApi.push(aux[i][j]);
+        }
+
+    }
+    occupationsApi = occupationsApi.filter((el, i)=>{
+        return occupationsApi.indexOf(el) === i;
+    })
+
+    occupationsApi.forEach(el => {
+        Occupation.findOrCreate({
+            where: {name: el}
+        })
+    });
+    const occupationsDb = await Occupation.findAll()
+    res.send(occupationsDb)
+})
+
+router.get('/character/:id', async (req, res) => {
+    const id = req.params.id;
+    if(!isNaN(id)){
+        const characterIdUrl = await axios.get('https://www.breakingbadapi.com/api/characters/' + id);
+        const characterIdInfo = await characterIdUrl.data.map (el => {
+            const { char_id, name, nickname, birthday, status, img, occupation } = el;
+            return {
+                char_id,
+                name,
+                nickname,
+                birthday,
+                status,
+                img,
+                occupation
+            }
+        })
+        res.send(characterIdInfo)
+    } else {
+        const characterIdBd = await Character.findByPk(id, {
+            include: 
+            {
+            model: Occupation,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            },
+            }});
+        res.send(characterIdBd);
+    } 
+})
+
+router.post('/character', async (req, res) => {
+    const { name, nickname, birthday, status, img, db, occupation } = req.body;
+
+    let charCreate = await Character.create({ name, nickname, birthday, status, img, db });
+    let occCreate = await Occupation.findAll({
+        where: {name: occupation}
+    })
+    charCreate.addOccupation(occCreate);
+    res.json('Recipe created');
+})
 
 module.exports = router;
